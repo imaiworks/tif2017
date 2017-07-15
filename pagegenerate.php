cbabead26778df40d8cc16bf39a472e881682877
root <root@centos69osx86.(none)>
2017-07-15 15:18:47 +0900

    13th commit

--- pagegenerate.php
+++ pagegenerate.php
@@ -1,5 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/app.css" />
 <title>TIF2017/TimeTableEditor</title>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script type="text/javascript" src="jquery.cookie.js"></script>
@@ -17,15 +19,23 @@ $(document).ready(function(){
 	$("#timetab").click( function(){
 		$("#artisttabsec").css("display","none");
 		$("#timetabsec").css("display","block");
+		$("#maptabsec").css("display","none");
 	});
 
 	$("#artisttab").click( function(){
 		$("#artisttabsec").css("display","block");
 		$("#timetabsec").css("display","none");
+		$("#maptabsec").css("display","none");
 	});
+	$("#maptab").click( function(){
+		$("#artisttabsec").css("display","none");
+		$("#timetabsec").css("display","none");
+		$("#maptabsec").css("display","block");
+	});
+	
 });
 </script>
-TIF2017/TimeTableEditor<br>
+<h1>TIF2017/TimeTableEditor</h1>
 <?php
 
 $r=file_get_contents("http://www.idolfes.com/2017/timetable/time.json");
@@ -44,6 +54,7 @@ foreach($tifdata as $key=>$data)
 		
 		foreach($tifdata[$key][$key2] as $key3=>$data3)
 		{
+			
 //			echo $key3."\n";
 			$data3["day"]=$key;
 			$data3["stage"]=$key2;
@@ -52,6 +63,35 @@ foreach($tifdata as $key=>$data)
 			
 			$artisttimelist[$tifdata[$key][$key2][$key3]["artist"]."--".$key."-".$tifdata[$key][$key2][$key3]["start"]."-".$tifdata[$key][$key2][$key3]["end"]]=$key."\t".$tifdata[$key][$key2][$key3]["start"]."\t".$tifdata[$key][$key2][$key3]["end"]."\t".$key2."\t".$tifdata[$key][$key2][$key3]["artist"];;
 			$artisttimelist_artistname[$tifdata[$key][$key2][$key3]["artist"]."--".$key."-".$tifdata[$key][$key2][$key3]["start"]."-".$tifdata[$key][$key2][$key3]["end"]]=$tifdata[$key][$key2][$key3]["artist"];
+			
+			
+			if($data3["detail"]!="null")
+			{
+// 				echo "<pre>";
+// 				print_r($data3);
+// 				echo "</pre>";
+				$detailarray=array();
+				$w_detailarray=explode("<br>",$data3["detail"]);
+				foreach($w_detailarray as $key4=>$data4)
+				{
+					$data4=str_replace(" ほか","",$data4);
+					if($data4=="")
+						$data4=$tifdata[$key][$key2][$key3]["artist"];
+					
+					$aname=$data4."(ｲﾍﾞﾝﾄ/ｼﾞｬﾝﾎﾞﾘｰ系)";
+					if(strstr($data4,"ゲスト"))
+						$aname=$data4;
+					$w_artistlist[$aname][]=$data3;
+					$artistlistname[$aname]=$aname;
+					$artisttimelist[$aname."--".$key."-".$tifdata[$key][$key2][$key3]["start"]."-".$tifdata[$key][$key2][$key3]["end"]]=$key."\t".$tifdata[$key][$key2][$key3]["start"]."\t".$tifdata[$key][$key2][$key3]["end"]."\t".$key2."\t".$aname;;
+					$artisttimelist_artistname[$aname."--".$key."-".$tifdata[$key][$key2][$key3]["start"]."-".$tifdata[$key][$key2][$key3]["end"]]=$tifdata[$key][$key2][$key3]["artist"];
+					
+					$eventname[$aname]=$tifdata[$key][$key2][$key3]["artist"];
+				}
+			}
+			
+			
+			
 		}
 			
 	}
@@ -89,28 +129,24 @@ $(document).ready(function(){
 </script>
 <?php 
 
-echo "<select id='selectidollist'>";
+echo "<select id='selectidollist' style='margin-bottom: 0px;'>";
 foreach($artistlistname as $key=>$data)
 {
 	echo "<option value='".md5($data)."'>".$data."</option>\n";
 }
 echo "</select>";
-echo "<button class='add'>アイドル/イベント表示追加</button>";
+echo "<button class='add' style='padding-top: 8px;padding-bottom: 8px;'>アイドル/イベント表示追加</button>";
 
 echo "<br>\n";
-echo "<div style='background-color:#ffeeff;width:50%;float:left;' id='timetab'>";
-echo "<div style='padding:15px;border: solid #008080;'>時間別</div>\n";
-echo "</div>\n";
-
-echo "<div>";
-echo "<div style='background-color:#eeffff;width:50%;float:left;' id='artisttab'>";
-echo "<div style='padding:15px;border: solid #008080;'>アイドル別</div>\n";
-echo "</div>\n";
-
-echo "<div style='clear:both;'></div>\n";
+?>
+<div style="display:table">
+<button class="button radius highlight" id='timetab'   style='float:left;margin-bottom: 0px;padding-left: 10px;padding-right: 10px;margin-right: 2px;background-color:#ffeeff;overflow:hidden;'>時間別</button>
+<button class="button radius" id='artisttab' style='float:left;margin-bottom: 0px;padding-left: 10px;padding-right: 10px;overflow:hidden;margin-right: 2px;'>アイドル・イベント別</button>
+<button class="button radius warning" id='maptab' style='float:left;margin-bottom: 0px;padding-left: 10px;padding-right: 10px;overflow:hidden;margin-right: 2px;'>地図</button>
+</div>
+<?php 
 
 
-echo "</div>";
 
 
 		
@@ -123,9 +159,13 @@ foreach($artisttimelist as $key=>$data)
 {
 	$arraydata=explode("\t",$data);
 	
-	echo "<div class='".md5($arraydata[4])."' style='display:none;border: solid #008080;'>";
-	echo "<div style='float:left;'>".$arraydata[4]." (".count($artistlist[$arraydata[4]]).")</div> <div  style='float:left;' class='del' value='".md5($arraydata[4])."'>[x]</div><div style='clear:both;'></div>\n";
-	echo "<div>".$arraydata[0]." ".substr($arraydata[1],0,2).":".substr($arraydata[1],2,2)."〜".substr($arraydata[2],0,2).":".substr($arraydata[2],2,2)." ".$arraydata[3]."</div>\n";
+	echo "<div class='".md5($arraydata[4])."' style='display:none;border: solid 2px #ec99f1;'>";
+	echo "<div style='float:left;'><h4 style='margin-top: 1px;margin-bottom: 0px;'>".$arraydata[4]." (".count($artistlist[$arraydata[4]]).")</h4></div> <div  style='float:left;' class='del ' value='".md5($arraydata[4])."'><span class='round alert label'>x</span></div><div style='clear:both;'></div>\n";
+	if(isset($eventname[$arraydata[4]]))
+		echo "<h4 style='margin-top: 1px;margin-bottom: 0px;' >".$eventname[$arraydata[4]]."</h4>";
+		
+	echo "<div><h5  style='margin-bottom: 0px;' >".$arraydata[0]." ".substr($arraydata[1],0,2).":".substr($arraydata[1],2,2)."〜".substr($arraydata[2],0,2).":".substr($arraydata[2],2,2)." ".$arraydata[3]."</h5>";
+	echo "</div>\n";
 	echo "</div>\n";
 }
 echo "</div>\n";
@@ -134,30 +174,41 @@ echo "<div id='artisttabsec' style='background-color:#eeffff;display:none;'>";
 
 foreach($artistlistname as $key=>$data)
 {
-	echo "<div class='".md5($key)."' style='display:none;border: solid #008080;'>";
-	echo "<div style='float:left;'>".$key." (".count($artistlist[$key]).")</div> <div style='float:left;' class='del' value='".md5($key)."'>[x]</div><div style='clear:both;'></div>\n";
+	echo "<div class='".md5($key)."' style='display:none;border: solid 2px #99ecf1;'>";
+	echo "<div style='float:left;'><h4 style='margin-top: 1px;margin-bottom: 0px;'>".$key." (".count($artistlist[$key]).")</h4></div> <div style='float:left;' class='del active' value='".md5($key)."'><span class='round alert label'>x</span><dd></div><div style='clear:both;'></div>\n";
 	foreach($artistlist[$key] as $key2=>$data2)
 	{
-		echo "<div>".$data2["day"]." ".substr($data2["start"],0,2).":".substr($data2["start"],2,2)."〜".substr($data2["end"],0,2).":".substr($data2["end"],2,2)." ".$data2["stage"]."</div>\n";
+		if(isset($eventname[$arraydata[4]]))
+			echo "<h4 style='margin-top: 1px;margin-bottom: 0px;' >".$eventname[$arraydata[4]]."</h4>";
+			
+		echo "<div><h5  style='margin-bottom: 0px;' >".$data2["day"]." ".substr($data2["start"],0,2).":".substr($data2["start"],2,2)."〜".substr($data2["end"],0,2).":".substr($data2["end"],2,2)." ".$data2["stage"]."</h5></div>\n";
 	}
 	echo "</div>\n";
 }
 echo "</div>\n";
 ?>
+<div id='maptabsec' style='background-color:#eeffff;display:none;'>
+<img src=http://www.idolfes.com/2017/images/map.jpg><br>
+<a target='_blank' href='http://www.idolfes.com/2017/'><h5>TIF2017 公式サイト</h5></a>
+</div>
+
+<br><br>
+<blockquote>
 アイドル/イベントを選択してボタンを押すと表示追加していきます<br>
-[x]を押すと表示から消えます<br>
+<span class='round alert label'>x</span>を押すと表示から消えます<br>
 選択したアイドル/イベントはブラウザに保存されますので、次このサイトに同じスマホ・PCの同じブラウザで訪れたときは前回の情報が反映されます。<br>
 
 アイドル/イベント単位での表示・非表示なので、時間ごとに表示・非表示はできません.ごめんなさい<br>
-<br>
 
-このタイムテーブルエディタを使って効率よくたくさんのアイドルを応援していきましょう！<br>
+<hr>
+タイムテーブルエディタを使って効率よくたくさんのアイドルを応援していきましょう！<br>
+<hr>
 <br>
 このサイトはTokyoIdolFestivalとは全く無関係です。<br>
 このサイトについての内容や問題などはTokyoIdolFestivalには絶対にお問い合わせをしないでください。お願いします。<br>
 このサイトのデータや内容に関しては必ずしも正しいものとは限りません。主催者発表の資料を必ず参照してください。このツールの製作者は一切の責任を負いかねます。ご理解の程よろしくお願いいたします。<br>
 
-
+</blockquote>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
